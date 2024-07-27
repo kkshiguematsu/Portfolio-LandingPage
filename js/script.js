3b38d080836e25967ef4d357e29174bb930dfb3b
kassianoshiguematsu <kassiano@weedvision.com.br>
2024-07-27 08:47:52 -0300

    UPDATE timeline

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
 
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', () => {
         const section = document.getElementById('about');
         const sectionPosition = section.offsetTop;
         
         var nav = document.getElementById('nav');
-        var navbar = document.getElementById('navbar');
         
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        var scrollTop =  document.documentElement.scrollTop;
         if (scrollTop !== 0) {
             nav.classList.remove("w-full")
             nav.classList.remove("bg-transparent")
+            nav.classList.add("shadow-lg") 
             nav.classList.add("w-[19.3rem]")
             nav.classList.add("sm:w-[23.3rem]")
             nav.classList.add("bg-neutral-900/70")
-            nav.classList.add("shadow-lg") 
         } else {
-            nav.classList.remove("w-[19.3rem]")
-            nav.classList.remove("sm:w-[23.3rem]")
-            nav.classList.remove("shadow-lg")
-            nav.classList.remove("bg-neutral-900/70")
             nav.classList.add("w-full")
             nav.classList.add("bg-transparent")
+            nav.classList.remove("shadow-lg")
+            nav.classList.remove("w-[19.3rem]")
+            nav.classList.remove("sm:w-[23.3rem]")
+            nav.classList.remove("bg-neutral-900/70")
         }
     });
     
@@ -37,49 +36,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const stickyRect = stickyDiv.getBoundingClientRect();
         const newHeight = stickyRect.top - parentRect.top;
         growingDiv.style.height = `${newHeight + 8}px`;
-        console.log(growingDiv.style.height)
     });
 
-    // document.querySelectorAll('.nav-link').forEach(link => {
-    //     link.addEventListener('click', function(event) {
-    //         if(window.innerWidth > 700){
-    //             event.preventDefault();
-    //             const targetId = this.getAttribute('href').substring(1);
-    //             const targetElement = document.getElementById(targetId);
-    //             const targetPosition = targetElement.getBoundingClientRect().top;
-    //             const offsetPosition = targetPosition - (window.innerHeight / 2) + (targetElement.offsetHeight / 2);
-    //             window.scrollTo({
-    //                 top: offsetPosition,
-    //                 behavior: 'smooth'
-    //             });
-    //         }
-    //     });
-    // });
-    
-    // window.addEventListener('scroll', function() {
-    //     var socialMediaLinks = document.getElementById("social-media-links");
-    //     const originalPosition = socialMediaLinks.offsetTop
-    //     console.log(window.pageYOffset)
-    //     console.log(originalPosition)
-    //     if (window.pageYOffset > originalPosition) {
-    //         socialMediaLinks.classList.remove("text-5xl")
-    //         socialMediaLinks.classList.remove("absolute")
-    //         socialMediaLinks.classList.remove("sm:bottom-0")
-    //         socialMediaLinks.classList.remove("bottom-5")
-    //         socialMediaLinks.classList.add("text-3xl")
-    //         socialMediaLinks.classList.add("fixed")
-    //     }  
-    //     if (window.pageYOffset < originalPosition) {
-    //         socialMediaLinks.classList.add("text-5xl")
-    //         socialMediaLinks.classList.add("absolute")
-    //         socialMediaLinks.classList.add("sm:bottom-0")
-    //         socialMediaLinks.classList.add("bottom-5")
-    //         socialMediaLinks.classList.remove("text-3xl")
-    //         socialMediaLinks.classList.remove("fixed")
-    //     }
-    // });
-
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', () => {
         const sections = document.querySelectorAll('section');
         const navLinks = document.querySelectorAll('.nav-link');
 
